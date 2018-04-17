29b74bd05fd369f82622995c40634a635ac4cfdd
adapthilario <33630246+adapthilario@users.noreply.github.com>
2018-04-17 11:48:27 +0900

    Create Login and Signup Screen

--- App.js
+++ App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 
-import FriendEditScreen from './src/screens/FriendEditScreen';
+import SignupScreen from './src/screens/SignupScreen';
 import CircleButton from './src/elements/CircleButton';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <FriendEditScreen />
+        <SignupScreen />
       </View>
     );
   }
---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import { StyleSheet, View, Text, TextInput, TouchableHighlight } from 'react-native';
+
+import Appbar from '../components/Appbar';
+
+
+class SignupScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Appbar />
+        <View style={styles.loginContent}>
+          <Text style={styles.title}>Join Frever!</Text>
+          <TextInput style={styles.input} value="Email Address" />
+          <TextInput style={styles.input} value="Password" />
+          <TouchableHighlight
+           style={styles.button}
+           onPress={() => {}}
+           underlayColor='#e25a00'
+          >
+           <Text style={styles.buttonText}> Signup! </Text>
+          </TouchableHighlight>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+  },
+  title: {
+    fontSize: 28,
+    alignSelf: 'center',
+    marginBottom: 24,
+  },
+  loginContent: {
+    padding: 24,
+    backgroundColor: '#fff',
+  },
+  input: {
+    backgroundColor: '#eee',
+    height: 48,
+    marginBottom: 16,
+    borderWidth: 1,
+    borderColor: '#DDD',
+    padding: 8,
+  },
+  button: {
+    backgroundColor: '#ff6600',
+    height: 48,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '67%',
+    alignSelf: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize:18,
+  },
+});
+
+export default SignupScreen;
